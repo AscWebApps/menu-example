07ef5f229a590ebf9fdb08c1d06d696e358d610c
Antonino <antoninoscaffidi@gmail.com>
2022-10-03 11:16:59 +0200

    Added in home page a simple hotwire example

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'home/index'
+  post 'home/add', as: :add
+  post 'home/remove', as: :remove
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
